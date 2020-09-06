cb40d835b987d17bf01890f7c804b4100d336c3f
Reed Santos <reedsantos@icloud.com>
2020-09-06 12:58:09 -0400

    add comments

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+// dependincies
 const express = require("express");
 const apiRoutes = require('./routing/apiRoutes');
 const htmlRoutes = require('./routing/htmlRoutes');
 
+// create an express server on port 3000
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// handle data parsing and routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
+// starts server
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
 });
