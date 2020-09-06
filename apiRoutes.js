cc1de4491168db599e1ba2927dcab9648a1b4483
Reed Santos <reedsantos@icloud.com>
2020-09-06 12:41:22 -0400

    dependencies and get request

---
+++ apiRoutes.js
@@ -0,0 +1,12 @@
+const Db = require('../db/db');
+const fs = require('fs');
+const uuidv1 = require('uuidv1');
+const router = require('express').Router();
+
+router.get("/notes", function (req, res) {
+    Db.getNote()
+        .then((note) => res.json(note))  
+        .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
