11bf3506cf9b8beda30249b24dc67010aa29c0d6
Reed Santos <reedsantos@icloud.com>
2020-09-06 12:58:33 -0400

    add comments

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,19 +1,23 @@
+// dependincies
 const Db = require('../db/db');
 const fs = require('fs');
 const uuidv1 = require('uuidv1');
 const router = require('express').Router();
 
+// get request that returns information in db.json
 router.get("/notes", function (req, res) {
     Db.getNote()
         .then((note) => res.json(note))  
         .catch((err) => res.status(500).json(err));
 });
+// post request captures user input and puts it into db.json
 router.post("/notes", (req, res) => {
     Db.addNote(req.body)
         .then((note) => res.json(note))
         .catch((err) => res.status(500).json(err));
 });
 
+// deletes the object with the corresponding id
 router.delete("/notes/:id", function (req, res) {
    console.log("delete route trigger")
     console.log(req.params.id)
