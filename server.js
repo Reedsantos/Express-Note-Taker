eec55a5f267426e777724068568985fd7a356ff1
Reed Santos <reedsantos@icloud.com>
2020-09-06 12:39:30 -0400

    add parsing and routes

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const apiRoutes = require('./routing/apiRoutes');
+const htmlRoutes = require('./routing/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
-})
+});
