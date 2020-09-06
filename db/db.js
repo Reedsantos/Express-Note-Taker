8510cb13e2ef80a68b3b08b376862c4e0808d89f
Reed Santos <reedsantos@icloud.com>
2020-09-06 12:59:27 -0400

    add comments

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
+// dependincies
 const util = require('util');
 const fs = require('fs');
 const uuidv1 = require('uuidv1');
-
+// convert readFile and writeFile to a promise based method
 const readData = util.promisify(fs.readFile);
 const writeData = util.promisify(fs.writeFile);
 
@@ -12,6 +13,7 @@ class Db {
     write(note){
         return writeData("db/db.json", JSON.stringify(note));
     }
+    // parse notes so it returns as an object
     getNote(){
         return this.read()
         .then (notes => {
@@ -28,13 +30,16 @@ class Db {
     
     addNote(note){
         const {title, text} = note;
+        // use uuidv1 to give a unique ID
         const newNote = {title, text, id: uuidv1()};
         return this.getNote()
+        // adds the new note to the end of the array
         .then (notes => [...notes, newNote]) 
         .then (writeNote => this.write(writeNote)
         .then (() => newNote)
         )
     }
+    // removes the object with the corresponding id from the array 
     removeNote(id){
         return this.getNote()
         .then (function (allNotes) {
